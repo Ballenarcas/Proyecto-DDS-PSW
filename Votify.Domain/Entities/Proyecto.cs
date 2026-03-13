a6ff94262cc14d22bc2ec75faf00af09e56ac160
Samuel Jimenez Gallega <SJIMGAL@upv.edu.es>
2026-03-13 11:59:40 +0100

    Clase proyecto añadida

---
+++ Votify.Domain/Entities/Proyecto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Proyecto
+{
+	public string Id { get; set; }
+	public string Categoria_Id { get; set; }
+	public string Nombre { get; set; }
+	public string Descripcion { get; set; }
+	public string Equipo_Id { get; set; }
+
+    protected Proyecto(string id, string nombre, string descripcion, string equipo)
+	{
+		Id = id;
+		Nombre = nombre;
+		Descripcion = descripcion;
+		Equipo_Id = equipo;
+    }
+}
